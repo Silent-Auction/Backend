e6a717ee42f9071bfb7c4cd8d8996c6a39953cae
Jordan <jordandoan@hotmail.com>
2019-10-21 10:49:10 -0700

    Added foriegn key constraint

--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,13 @@ module.exports = {
     },
     seeds: {
       directory: './data/seeds'
-    }
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
   },
 
   staging: {
