bafdd765247a37191f194dd1aa0cedd014d1d069
Jordan <jordandoan@hotmail.com>
2019-10-22 10:40:35 -0700

    added knex cleaner

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -2,15 +2,10 @@
 const bcrypt = require("bcryptjs");
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('users').truncate()
-    .then(function () {
-      // Inserts seed entries
-      return knex('users').insert([
+  return knex('users').insert([
         {username: "testseller", password: bcrypt.hashSync("password", 12), first_name: "Test", last_name: "Seller", is_seller: true},
         {username: "testseller2", password: bcrypt.hashSync("password", 12), first_name: "Test", last_name: "Seller2", is_seller: true},
         {username: "testbuyer", password: bcrypt.hashSync("password", 12), first_name: "Test", last_name: "Buyer", is_seller: false},
         {username: "testbuyer2", password: bcrypt.hashSync("password", 12), first_name: "Test", last_name: "Buyer2", is_seller: false},
       ]);
-    });
 };
