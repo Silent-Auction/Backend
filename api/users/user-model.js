8a2146c6a9fd92dce4548d06a0b7a43045a4be94
Jordan <jordandoan@hotmail.com>
2020-03-06 16:06:09 -0800

    Hopefuly fixed buyer auctions stuff

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -24,7 +24,18 @@ function getBuyerAuctions(user_id) {
       .whereRaw(`auction_bids.user_id = ${user_id}`)
       .join('auctions as a', 'auction_bids.auction_id', 'a.id')
       .join('users as u', 'a.user_id', 'u.id')
-      .select('a.id as auction_id', 'u.username as sold_by_user', 'u.first_name as sold_by', 'a.name', 'a.description', 'a.date_starting', 'a.date_ending', 'a.image', 'auction_bids.id as bid_id', 'auction_bids.created_at as bid_date')
-      .max('auction_bids.price as your_recent_bid')
-      .groupBy('a.id', 'u.username')
+      .select('a.id as auction_id', 'u.username as sold_by_user', 'u.first_name as sold_by', 'a.name', 'a.description', 'a.date_starting', 'a.date_ending', 'a.image',)
+      .then(async auctions => {
+        const newAuctions = auctions.map(async auction => {
+          const bid = await db('auction_bids')
+            .where({auction_id: auction.auction_id})
+            .andWhere({user_id})
+            .max('price as your_recent_bid', 'id as bid_id', 'created_at as bid_date')
+            .groupBy('auction_id')
+            .first()
+          const new_auction = {...auction, ...bid}
+          return new_auction
+        })
+        return Promise.all(newAuctions)
+      })
 }
